ab2c8e42079a051f052ff877380c34ad37b825bf
Lars Peter Søndergaard <nitori@chireiden.net>
2015-10-25 20:28:26 +0100

    [TASK] More structured repr of 005 messages.

--- shanghai/irc/messages/core.py
+++ shanghai/irc/messages/core.py
@@ -1,7 +1,9 @@
 
 from . import Message, register_derivative
 from .. import parse
+from itertools import zip_longest
 import re
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -30,8 +32,19 @@ class Numeric005(Message, metaclass=register_derivative):
             if key == 'PREFIX':
                 match = re.match(r'\((.*)\)(.*)$', value)
                 self.options[key] = tuple(match.groups())
+            elif key == 'CHANMODES':
+                self.options[key] = tuple(value.split(','))
             else:
                 self.options[key] = value
 
     def __repr__(self):
-        return '005[{!r}]'.format(self._pkg)
+        chunks = zip_longest(*[iter(self.options)]*3)
+        output = ''
+        for cols in chunks:
+            row = []
+            for key in cols:
+                if key is None:
+                    break
+                row.append('{:<50}'.format(key+'='+str(self.options[key])))
+            output += '    {}\n'.format(' '.join(row))
+        return '<005>\n{}'.format(output)
